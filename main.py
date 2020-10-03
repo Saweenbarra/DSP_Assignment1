e965711f872394c26363ce044069f6734ade7202
Adam Frew <adamfrew1999@gmail.com>
2020-10-03 18:26:44 +0100

    Commented code

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import numpy as np
 import scipy.io.wavfile as wavfile
 import matplotlib.pyplot as plt
 
-
+#Load wav file sample frequency and data
 fs, data = wavfile.read("Potatoes.wav")
 
+#Calculate duration of sample and create time (x-axis) array
 duration = len(data)/fs
 t = np.linspace(0, duration, len(data))
 
+#Plot sample in time domain
 plt.figure()
 plt.plot(t, data)
 plt.show()
