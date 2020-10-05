59832a7ae8128a044a72680d43a73b751a2c185b
Adam Frew <adamfrew1999@gmail.com>
2020-10-05 19:20:31 +0100

    Boost signal and write wav file

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import scipy.io.wavfile as wavfile
 import matplotlib.pyplot as plt
 
 #Load wav file sample frequency and data
-fs, data = wavfile.read("Vowel.wav")
+fs, data = wavfile.read("Potatoes.wav")
 
 #Calculate duration of sample and create time (x-axis) array
 duration = len(data)/fs
@@ -15,13 +15,44 @@ tplot = plt.plot(t, data)
 tplot = plt.xlabel("Time (s)")
 tplot = plt.ylabel("Amplitude")
 
-#Plot sample in frequency domain log(x-axis) and log(y-axis)
+#Calculate discreet fourier transform of data
 dataf = np.fft.fft(data)
+#Create frequency axis
 f = np.linspace(0, fs, len(dataf))
+
+#Plot sample in frequency domain log(x-axis) and log(y-axis)
 plt.figure(2)
 fplot = plt.plot(f, abs(dataf))
 fplot = plt.xlabel("Frequency (Hz)")
 fplot = plt.ylabel("Magnitude")
 plt.xscale("log")
 plt.yscale("log")
+
+#Find sample numbers corresponding to 6kHz and 10kHz
+k1 = int(len(dataf)/44100*6000)
+k2 = int(len(dataf)/44100*10000)
+
+#Amplify selected frequencies by factor
+dataf[k1:k2+1] *= 1.00001
+
+#Plot new amplified FFT
+plt.figure(3)
+bfplot = plt.plot(f, abs(dataf))
+bfplot = plt.xlabel("Frequency (Hz)")
+bfplot = plt.ylabel("Magnitude")
+plt.xscale("log")
+plt.yscale("log")
+
+#Convert boosted signal to time domain
+dataBoost = np.fft.ifft(dataf)
+dataBoost = np.real(dataBoost)
+
+#Plot boosted signal in time domain
+plt.figure(4)
+btplot = plt.plot(t, dataBoost)
+btplot = plt.xlabel("Time (s)")
+btplot = plt.ylabel("Amplitude")
+
+#Write boosted signal to wav file
+wavfile.write("PotatoesBoost.wav", fs, dataBoost)
 plt.show()
