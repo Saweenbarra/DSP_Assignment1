Fixed normalisation of time domain and freq now in dB

voice_enhancer.py
@@ -9,7 +9,8 @@ fs, data = wavfile.read("original.wav")
 duration = len(data)/fs
 t = np.linspace(0, duration, len(data))
 
-normaliser = (pow(2,16)-1)
+#Normalising factor
+normaliser = (pow(2,15)-1)
 
 #Plot sample in time domain
 plt.figure(1)
@@ -17,26 +18,21 @@ tplot = plt.plot(t, data/normaliser)
 tplot = plt.xlabel("Time (s)")
 tplot = plt.ylabel("Amplitude")
 
-#Calculate discreet fourier transform of data
+#Calculate the fast fourier transform of data
 dataf = np.fft.fft(data)
+#Convert FFT data to decibels relative to full scale (dBFS)
+datafdB = 20*np.log10(abs(dataf)*2/len(dataf)/(pow(2,15)-1))
+
 #Create frequency axis
 f = np.linspace(0, fs, len(dataf))
 
-#Plot sample in frequency domain log(x-axis) and log(y-axis)
+#Plot sample in frequency domain
 plt.figure(2)
-fplot = plt.plot(f, abs(dataf)/len(dataf))
+fplot = plt.plot(f, datafdB)
 fplot = plt.xlabel("Frequency (Hz)")
-fplot = plt.ylabel("Magnitude")
+fplot = plt.ylabel("dBFS")
 plt.xscale("log")
-plt.yscale("log")
-plt.xlim(right = fs/2)
-
-#Find sample numbers corresponding to 6kHz and 10kHz
-k1 = int(len(dataf)/fs*6000)
-k2 = int(len(dataf)/fs*10000)
-
-#Amplify selected frequencies by factor
-dataf[k1:k2+1] *= 20
+plt.xlim(right = fs/2) #Don't plot data after nyquist because it doesn't exist
 
 #Find sample numbers corresponding to 85Hz and 180Hz
 k1 = int(len(dataf)/fs*85)
@@ -45,14 +41,23 @@ k2 = int(len(dataf)/fs*180)
 #Amplify selected frequencies by factor
 dataf[k1:k2+1] *= 5
 
+#Find sample numbers corresponding to 6kHz and 10kHz
+k1 = int(len(dataf)/fs*6000)
+k2 = int(len(dataf)/fs*10000)
+
+#Amplify selected frequencies by factor
+dataf[k1:k2+1] *= 20
+
+#Convert FFT data to decibels relative to full scale (dBFS)
+datafdB = 20*np.log10(abs(dataf)*2/len(dataf)/normaliser)
+
 #Plot new amplified FFT
 plt.figure(3)
-bfplot = plt.plot(f, abs(dataf)/len(dataf))
+bfplot = plt.plot(f, datafdB)
 bfplot = plt.xlabel("Frequency (Hz)")
-bfplot = plt.ylabel("Magnitude")
+bfplot = plt.ylabel("dBFS")
 plt.xscale("log")
-plt.yscale("log")
-plt.xlim(right = fs/2)
+plt.xlim(right = fs/2) #Don't plot data after nyquist because it doesn't exist
 
 #Convert boosted signal to time domain
 dataBoost = np.fft.ifft(dataf)
@@ -63,7 +68,7 @@ dataBoost = dataBoost.astype(np.int16)
 
 #Plot boosted signal in time domain
 plt.figure(4)
-btplot = plt.plot(t, dataBoost/normaliser)
+btplot = plt.plot(t, dataBoost/(pow(2,15)-1))
 btplot = plt.xlabel("Time (s)")
 btplot = plt.ylabel("Amplitude")
 