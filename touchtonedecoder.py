da571ce297411b4a82a67d85790dd13d9c3386a3
Lewis Russell <lewisrussell@Lewiss-MacBook-Pro-2.local>
2020-10-15 16:57:06 +0100

    cleaned up touchtones

--- touchtonedecoder.py
+++ touchtonedecoder.py
@@ -4,44 +4,34 @@ import matplotlib.pyplot as plt
 
 fs = 1000
 touchtones = np.genfromtxt("./touchtones.dat",dtype=None,delimiter=' ')
-#touchtones_t, touchtones_m = np.hsplit(touchtones,2)
-touchtones_m = touchtones[:,1]
-t = touchtones[:,0]
+touchtones[:,1] = touchtones[:,1].astype(np.int16)
 
-touchtones_m = touchtones_m.astype(np.int16)
+wavfile.write("touchtones.wav", fs, touchtones[:,1])
+
+oldi = 0
+seperate_tones = []
+j = 0
+for i in range(len(touchtones[:,1])):
+    if(touchtones[:,1][i] > 3300 or touchtones[:,1][i] < 3200):
+        j = 0    
+    if(touchtones[:,1][i] < 3300 and touchtones[:,1][i] > 3200):
+        j += 1
+        if j > 10:
+            if i > oldi + 20:
+                seperate_tones.append(touchtones[oldi:(i-10)])
+            oldi = i
+            
 
 plt.figure(1)
-
-wavfile.write("output.wav", fs, touchtones_m)
-
-tplot = plt.plot(t, touchtones_m)
-tplot = plt.xlabel("Time (s)")
+tplot = plt.plot(touchtones[:,0], touchtones[:,1])
+for i in range(len(seperate_tones)):
+    tplot = plt.plot(seperate_tones[i][:,0], seperate_tones[i][:,1],'r')
+tplot = plt.xlabel("Time (ms)")
 tplot = plt.ylabel("Amplitude")
-#plt.show()
-
-def Dial_detector (touchtones_m):
-    oldi = 0
-    seperate_tones = []
-    j = 0
-    for i in range(len(touchtones_m)):
-        if(touchtones_m[i] > 3300 or touchtones_m[i] < 3200):
-            j = 0    
-        if(touchtones_m[i] < 3300 and touchtones_m[i] > 3200):
-            j += 1
-            if j > 10:
-                if i > oldi + 20:
-                    seperate_tones.append(touchtones[oldi:(i-10)])
-                oldi = i
-                
-    print(seperate_tones)
-    for i in range(len(seperate_tones)):
-        tplot = plt.plot(seperate_tones[i][:,0], seperate_tones[i][:,1],'r')
-    tplot = plt.xlabel("Time (ms)")
-    tplot = plt.ylabel("Amplitude")
-    plt.show()
+plt.show()
 
 
 
-Dial_detector(touchtones_m)
+Dial_detector(touchtones[:,1])
 
 #def Dial_Identifier():
