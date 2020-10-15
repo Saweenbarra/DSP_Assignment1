0ab910a10b3ea77f89ca7a0bd54cd7801f238d55
Lewis Russell <lewisrussell@Lewiss-MacBook-Pro-2.local>
2020-10-15 20:44:17 +0100

    halp 2.0

--- touchtonedecoder.py
+++ touchtonedecoder.py
@@ -1,9 +1,10 @@
 import numpy as np
 import scipy.io.wavfile as wavfile
 import matplotlib.pyplot as plt
+import math
 
 fs = 1000
-touchtones = np.genfromtxt("./touchtones.dat",dtype=None,delimiter=' ')
+touchtones = np.genfromtxt("touchtones.dat",dtype=None,delimiter=' ')
 touchtones[:,1] = touchtones[:,1].astype(np.int16)
 
 wavfile.write("touchtones.wav", fs, touchtones[:,1])
@@ -11,19 +12,28 @@ wavfile.write("touchtones.wav", fs, touchtones[:,1])
 oldi = 0
 seperate_tones = []
 j = 0
+freqs = [303, 230, 148, 59, 209, 336, 447]
 
 def Tone_ID(touchtone):
     f_touchtone = np.fft.fft(touchtone)
     f = np.linspace(0, fs, len(f_touchtone))
+    k1 = int(len(f_touchtone)/fs*500)
+    
     f_touchtonedB = 20*np.log10(abs(f_touchtone)*2/len(f_touchtone)/(pow(2,15)-1))
+    peaks = np.where(f_touchtonedB[1:k1] > np.max(f_touchtonedB[1:k1])-10)
+    peaks = list(peaks)
+    for i in range(len(peaks)):
+        peaks[i] = peaks[i]*fs/len(f_touchtone)
+        for j in range(len(freqs)):   
+            if math.isclose(peaks[i],freqs[j],rel_tol = 0.1):
+                peaks[i] = freqs[j]
 
+    print(peaks)
     plt.figure(2)
     fplot = plt.plot(f, f_touchtonedB)
     fplot = plt.xlabel("Frequency (Hz)")
     fplot = plt.ylabel("FS")
-    plt.xscale("log")
-    plt.xlim(right = fs/2)
-    plt.xlim(left = 10)
+    plt.xlim(right = fs/2,left = 1)
     
 
 for i in range(len(touchtones[:,1])):
@@ -34,14 +44,13 @@ for i in range(len(touchtones[:,1])):
         if j > 10:
             if i > oldi + 20:
                 seperate_tones.append(touchtones[oldi:(i-10)])
-                Tone_ID(touchtones[oldi:(i-10)][:,1])
+                #Tone_ID(touchtones[oldi:(i-10)][:,1])
             oldi = i
 
-
+Tone_ID(seperate_tones[0][:,1])
 plt.figure(1)
-print(len(seperate_tones))
 #tplot = plt.plot(touchtones[:,0], touchtones[:,1])
-for i in range(1):
+for i in range(len(seperate_tones)):
     tplot = plt.plot(seperate_tones[i][:,0], seperate_tones[i][:,1],'r')
 tplot = plt.xlabel("Time (ms)")
 tplot = plt.ylabel("Amplitude")
