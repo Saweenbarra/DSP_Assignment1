332a40a90cbe12c6584c339bf8c818a240b21a55
Lewis Russell <lewisrussell@Lewiss-MacBook-Pro-2.local>
2020-10-15 16:44:26 +0100

    Cut tones into seperate arrays w/ plot

--- touchtonedecoder.py
+++ touchtonedecoder.py
@@ -6,8 +6,7 @@ fs = 1000
 touchtones = np.genfromtxt("./touchtones.dat",dtype=None,delimiter=' ')
 #touchtones_t, touchtones_m = np.hsplit(touchtones,2)
 touchtones_m = touchtones[:,1]
-duration = len(touchtones_m)/fs
-t = np.linspace(0, duration, len(touchtones_m))
+t = touchtones[:,0]
 
 touchtones_m = touchtones_m.astype(np.int16)
 
@@ -18,18 +17,28 @@ wavfile.write("output.wav", fs, touchtones_m)
 tplot = plt.plot(t, touchtones_m)
 tplot = plt.xlabel("Time (s)")
 tplot = plt.ylabel("Amplitude")
-plt.show()
+#plt.show()
 
 def Dial_detector (touchtones_m):
     oldi = 0
-    seperate_tones = np.zeros([1,2])
+    seperate_tones = []
+    j = 0
     for i in range(len(touchtones_m)):
-        if(touchtones_m[i] < 3250 and touchtones_m[i] > 3200):
-            if i > (oldi + 1):
-                touchtone = touchtones[oldi:i]
-                seperate_tones = np.append(seperate_tones,touchtone,axis=0)
-            oldi = i
+        if(touchtones_m[i] > 3300 or touchtones_m[i] < 3200):
+            j = 0    
+        if(touchtones_m[i] < 3300 and touchtones_m[i] > 3200):
+            j += 1
+            if j > 10:
+                if i > oldi + 20:
+                    seperate_tones.append(touchtones[oldi:(i-10)])
+                oldi = i
+                
     print(seperate_tones)
+    for i in range(len(seperate_tones)):
+        tplot = plt.plot(seperate_tones[i][:,0], seperate_tones[i][:,1],'r')
+    tplot = plt.xlabel("Time (ms)")
+    tplot = plt.ylabel("Amplitude")
+    plt.show()
 
 
 
