e7e7b957e5ae21b0cd2c048986b43714e892ee15
Lewis Russell <lewisrussell@Lewiss-MacBook-Pro-2.local>
2020-10-15 22:41:57 +0100

    removed test code

--- touchtonedecoder.py
+++ touchtonedecoder.py
@@ -6,11 +6,7 @@ import math
 fs = 1000
 touchtones = np.genfromtxt("touchtones.dat",dtype=None,delimiter=' ')
 touchtones[:,1] = touchtones[:,1].astype(np.int16)
-
-wavfile.write("touchtones.wav", fs, touchtones[:,1])
-
 oldi = 0
-seperate_tones = []
 j = 0
 fd_freqs = [303, 230, 148, 59, 209, 336, 477] #fold down frequancies corresponding to tone frequancies
 keys = [['1','2','3','error'],
@@ -43,13 +39,6 @@ def Tone_ID(touchtone):
     
     print('key:',keys[indices[0]][indices[1]])
 
-    plt.figure(2)
-    fplot = plt.plot(f, f_touchtonedB)
-    fplot = plt.xlabel("Frequency (Hz)")
-    fplot = plt.ylabel("FS")
-    plt.xlim(right = fs/2,left = 1)
-    
-
 for i in range(len(touchtones[:,1])):
     if(touchtones[:,1][i] > 3300 or touchtones[:,1][i] < 3200):
         j = 0    
@@ -57,15 +46,5 @@ for i in range(len(touchtones[:,1])):
         j += 1
         if j > 10:
             if i > oldi + 20:
-                seperate_tones.append(touchtones[oldi:(i-10)])
                 Tone_ID(touchtones[oldi:(i-10)][:,1])
             oldi = i
-
-#Tone_ID(seperate_tones[0][:,1])
-plt.figure(1)
-tplot = plt.plot(touchtones[:,0], touchtones[:,1])
-for i in range(len(seperate_tones)):
-    tplot = plt.plot(seperate_tones[i][:,0], seperate_tones[i][:,1],'r')
-tplot = plt.xlabel("Time (ms)")
-tplot = plt.ylabel("Amplitude")
-#plt.show()
